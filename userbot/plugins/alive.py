8dc6a95645fa569cfe5555af1079d35099c9dbfd
rishabh-lohiya16 <63769221+rishabh-lohiya16@users.noreply.github.com>
2020-04-17 18:10:31 +0530

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,19 @@
+"""Check if userbot alive."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("`Jinda hu bhnchod` **ψ(｀∇´)ψ**\n\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
+                     "`Bot created by:` [OPen Camera](tg://user?id=772507084), @opencamera\n"
+                     f"`My Son`: {DEFAULTUSER}\n\n"
+                     "https://github.com/rishabh-lohiya16/Nandi"
+                     "Ayushman-Bhav✋")
