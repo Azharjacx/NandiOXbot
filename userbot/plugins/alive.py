d4825783e72e7ee9e6bf10a9e65b75deee343085
rishabh-lohiya16 <63769221+rishabh-lohiya16@users.noreply.github.com>
2020-04-17 18:58:37 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Jinda hu bhnchod` **ψ(｀∇´)ψ**\n\n"
+    await alive.edit("`Jinda hu bhnchod ` **ψ(｀∇´)ψ**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
                      "`Bot created by:` [OPen Camera](tg://user?id=772507084), @opencamera\n"
-                     f"`My Son`: {DEFAULTUSER}\n\n"
+                     f"`My NOOB Son`: {DEFAULTUSER}\n\n"
                      "https://github.com/rishabh-lohiya16/NandiOXbot"
-                     "Ayushman-Bhav✋")
+                     "\nAyushman-Bhav✋")
